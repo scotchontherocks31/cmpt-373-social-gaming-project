3342e60073db4e83dcff9584b2c541a593f474c8
Tung Huynh <tungh@sfu.ca>
2020-09-29 14:59:36 -0700

    add Room and User

--- tools/chatserver/chatserver.h
+++ tools/chatserver/chatserver.h
@@ -6,3 +6,17 @@ struct MessageResult {
   std::string result;
   bool shouldShutdown;
 };
+
+struct Room {
+	int id;
+	std::string roomName;
+	std::vector<User*> participants;
+	// etc...
+};
+
+struct User {
+	Connection connection;
+	std::string name;
+	Room* room;
+	// etc...
+};
