7883cee8079727f4ad5bfc20a104d8710946a155
aba122 <aba122@sfu.ca>
2020-10-04 12:01:11 -0700

    Mock ASTNode and ASTVisitor

--- AST/ASTNode.h
+++ AST/ASTNode.h
@@ -2,19 +2,32 @@
 #define AST_NODE_H
 
 #include <vector>
+#include <memory>
 
-template <typename T>
-class ASTVisitor;
+namespace AST {
 
 class ASTNode {
     public:
-        int numChildren() const;
-        ASTNode& getParent() const;
-        std::vector<ASTNode*> getChildren() const;
-        virtual void accept(ASTVisitor<void>&) = 0;
+        int numChildren() const {
+            return numChildren;
+        }
+        // check mutability
+        const std::vector<const ASTNode*> getChildren() const;
+        const ASTNode& getParent() const;
+        void setParent(ASTNode& parent);
+        void accept(ASTVisitor& visitor);
+        void setType(Type&);
+        Type& getType() const;
+        // Binding says variable available here scope (compile time)
+        const Binding& getBinding() const;
+        void setBinding(Binding&);
         virtual ~ASTNode();
-    protected:
+    private:
+        std::vector<std::unique_ptr<ASTNode>> children;
         ASTNode* parent;
+        int numChildren;
 };
+
+}
 
 #endif
--- AST/ASTVisitor.h
+++ AST/ASTVisitor.h
@@ -1,19 +1,66 @@
 #ifndef AST_VISITOR_H
 #define AST_VISITOR_H
 
-#include "ASTLoopNode.h"
-#include "ASTForEachNode.h"
+namespace AST {
 
-template<typename T>
-class ASTVisitor {
+class Communication {
     public:
-        virtual T visit(ASTLoopNode&);
-        virtual ~ASTVisitor() = 0;
-    protected:
-        virtual T visitLeave(ASTLoopNode&);
-        virtual T visitEnter(ASTLoopNode&);
-        virtual T visitLeave(ASTForEachNode&);
-        virtual T visitEnter(ASTForEachNode&);
+        sendGlobalMessage(std::string&);
 };
 
+class ASTVisitor {
+    public:
+        virtual void visitLeave(ForEachNode&);
+        virtual void visitEnter(ForEachNode&);
+        virtual void visitLeave(GlobalMessageNode&);
+        virtual void visitEnter(GlobalMessageNode&);
+        virtual void visitLeave(ListNode&);
+        virtual void visitEnter(ListNode&);
+        virtual void visitLeave(ElementNode&);
+        virtual void visitEnter(ElementNode&);
+        virtual void visitLeave(RulesNode&);
+        virtual void visitEnter(RulesNode&);
+        virtual void visitLeave(FormatNode&);
+        virtual void visitEnter(FormatNode&);
+        virtual ~ASTVisitor() = 0;
+    private:
+};
+
+class Interpreter : public ASTVisitor {
+    public:
+        virtual void visitLeave(ForEachNode&) override;
+        virtual void visitEnter(ForEachNode& node) override;
+        virtual void visitLeave(GlobalMessageNode&) override;
+        virtual void visitEnter(GlobalMessageNode&) override;
+        virtual void visitLeave(ListNode&) override;
+        virtual void visitEnter(ListNode&) override;
+        virtual void visitLeave(ElementNode&) override;
+        virtual void visitEnter(ElementNode&) override;
+        virtual void visitLeave(RulesNode&) override;
+        virtual void visitEnter(RulesNode&) override;
+        virtual void visitLeave(FormatNode&) override;
+        virtual void visitEnter(FormatNode&) override;
+        private:
+        Environment environment;
+        Communication &communication;
+};
+
+class DSLValue;
+
+class Environment {
+    public:
+        const DSLValue& getValue(ASTNode&) const;
+        void removeBinding(ASTNode&);
+        bool constains(ASTNode&);
+        void setBinding(ASTNode&, DSLValue);
+        Environment& createChildEnvironment();
+        void removeChildEnvironment();
+    private:
+        Environment *parent;
+        std::unique_ptr<Environment> child;
+        std::unordered_map<ASTNode*, DSLValue> bindings;
+};
+
+
+}
 #endif
