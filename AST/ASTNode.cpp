e5187102ab29fe56a92b0c97476daea9e05ab888
sarbjot-14 <sarbjot_14@hotmail.com>
2020-10-11 03:43:56 +0000

    reformat

--- AST/ASTNode.cpp
+++ AST/ASTNode.cpp
@@ -2,23 +2,18 @@
 #include "ASTVisitor.h"
 
 namespace AST {
-    void GlobalMessage::acceptHelper(ASTVisitor& visitor) {
-        visitor.visit(*this);
-    }
-    void GlobalMessage::acceptForChildrenHelper(ASTVisitor& visitor) {
-        for (auto& child : children) {
-            child->accept(visitor);
-        }
-    }
-
-    void FormatNode::acceptHelper(ASTVisitor& visitor) {
-        visitor.visit(*this);
-    }
-    void FormatNode::acceptForChildrenHelper(ASTVisitor& visitor) {
-        for (auto& child : children) {
-            child->accept(visitor);
-        }
-    }
-
-    
+void GlobalMessage::acceptHelper(ASTVisitor &visitor) { visitor.visit(*this); }
+void GlobalMessage::acceptForChildrenHelper(ASTVisitor &visitor) {
+  for (auto &child : children) {
+    child->accept(visitor);
+  }
 }
+
+void FormatNode::acceptHelper(ASTVisitor &visitor) { visitor.visit(*this); }
+void FormatNode::acceptForChildrenHelper(ASTVisitor &visitor) {
+  for (auto &child : children) {
+    child->accept(visitor);
+  }
+}
+
+} // namespace AST
