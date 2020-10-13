b242136b806c6bdaccf30f1f4be044d1adc5178d
sarbjot-14 <sarbjot_14@hotmail.com>
2020-10-13 19:01:15 +0000

    merged with develop

--- AST/Parser.h
+++ AST/Parser.h
@@ -10,41 +10,39 @@ class JSON;
 namespace AST {
 
 class DomainSpecificParser {
-    public:
-        AST parse() {
-            return parseHelper();
-        }
-        virtual ~DomainSpecificParser();
-    private:
-        virtual AST parseHelper() = 0;
+public:
+  AST parse() { return parseHelper(); }
+  virtual ~DomainSpecificParser();
+
+private:
+  virtual AST parseHelper() = 0;
 };
 
 class ASTParser {
-    private:
-        using DSP = DomainSpecificParser;
-    public:
-        ASTParser(std::unique_ptr<DSP>&& parser) : parser{std::move(parser)} {}
-        AST parse() {
-            return parser->parse();
-        }
-        void setParser(std::unique_ptr<DSP> &&parser) {
-            parser.swap(this->parser);
-        }
-    private:
-        std::unique_ptr<DSP> parser;
+private:
+  using DSP = DomainSpecificParser;
+
+public:
+  ASTParser(std::unique_ptr<DSP> &&parser) : parser{std::move(parser)} {}
+  AST parse() { return parser->parse(); }
+  void setParser(std::unique_ptr<DSP> &&parser) { parser.swap(this->parser); }
+
+private:
+  std::unique_ptr<DSP> parser;
 };
 
 class JSONToASTParser : public DomainSpecificParser {
-    public:
-        JSONToASTParser(const JSON& json) : json{json} {}
-    private:
-        const JSON &json;
-        // Implement these in a Top Down fashion
-        virtual AST parseHelper() override;
-        FormatNode parseFormatNode();
-        GlobalMessage parseGlobalMessage();
+public:
+  JSONToASTParser(const JSON &json) : json{json} {}
+
+private:
+  const JSON &json;
+  // Implement these in a Top Down fashion
+  virtual AST parseHelper() override;
+  FormatNode parseFormatNode();
+  GlobalMessage parseGlobalMessage();
 };
 
-}
+} // namespace AST
 
 #endif
