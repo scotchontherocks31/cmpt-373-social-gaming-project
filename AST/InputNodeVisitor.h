45bcbf09dbf176bc0b8116efcd55f0ec395a9afb
vnadtoch <vnadtoch@sfu.ca>
2020-10-21 18:44:50 -0700

    Aded files for InputVisitorNode class

---
+++ AST/InputNodeVisitor.h
@@ -0,0 +1,14 @@
+#ifndef INPUT_NODE_VISITOR_H
+#define INPUT_NODE_VISITOR_H
+#include "ASTNode.h"
+
+namespace AST
+{
+
+    class InputNodeVisitor : public AST::ASTVisitor
+    {
+        
+    };
+
+} // namespace AST
+#endif
