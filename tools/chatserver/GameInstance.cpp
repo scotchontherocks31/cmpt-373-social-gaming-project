0be4838e75422037b45565e4c69bd78b5617f62a
sarbjot-14 <sarbjot_14@hotmail.com>
2020-10-29 17:22:21 +0000

    merging with develop

---
+++ tools/chatserver/GameInstance.cpp
@@ -0,0 +1,62 @@
+#include "GameInstance.h"
+#include "GameServer.h"
+#include <algorithm>
+
+GameInstance::GameInstance(Room &room, GameServer &server)
+    : room{&room}, server{&server} {
+  int counter = 0;
+  for (auto &[id, user] : room.getMembers()) {
+    players.push_back({counter, user->name});
+    playerIdMapping.insert({counter, id});
+    playerMessageRequest.insert({counter, false});
+    reversePlayerIdMapping.insert({id, counter});
+    ++counter;
+  }
+}
+
+void GameInstance::sendToPlayer(const Player &player, std::string message) {
+  auto userId = playerIdMapping.at(player.id);
+  auto &user = room->getMember(userId);
+  server->sendMessageToUser(user, std::move(message));
+}
+
+void GameInstance::sendGlobalMessage(std::string message) {
+  server->sendMessageToRoom(*room, std::move(message));
+}
+
+std::deque<PlayerMessage>
+GameInstance::receiveFromPlayer(const Player &player) {
+  std::deque<PlayerMessage> messages;
+  auto &&[x, y] =
+      std::ranges::partition(inboundMessageQueue, [&player](auto &message) {
+        return player.id != message.player->id;
+      });
+  std::ranges::move(x, inboundMessageQueue.end(), std::back_inserter(messages));
+  inboundMessageQueue.erase(x, inboundMessageQueue.end());
+  playerMessageRequest.at(player.id) = messages.empty();
+  return messages;
+}
+
+bool GameInstance::queueMessage(const User &user, std::string message) {
+  if (gameTask.isDone()) {
+    return false;
+  }
+  auto playerId = reversePlayerIdMapping.at(user.getId());
+  auto &player = players.at(playerId);
+  if (!playerMessageRequest.at(playerId)) {
+    return false;
+  }
+  inboundMessageQueue.push_back({&player, std::move(message)});
+  return true;
+}
+
+void GameInstance::loadGame(AST::AST &ast, AST::Environment env) {
+  auto interpreter = AST::Interpreter{std::move(env), *this};
+  gameTask = ast.accept(interpreter);
+}
+
+void GameInstance::runGame() {
+  if (!gameTask.isDone()) {
+    gameTask.resume();
+  }
+}
---
+++ tools/chatserver/GameInstance.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "ASTVisitor.h"
+#include "Room.h"
+#include <list>
+#include <map>
+#include <string>
+
+class GameServer;
+class GameManager;
+
+/*
+TODO: Consider making message context aware so that a random message
+from player doesn't get mixed with an actual response.
+*/
+
+struct Player {
+  int id;
+  std::string name;
+};
+
+struct PlayerMessage {
+  Player *player;
+  std::string message;
+};
+
+class GameInstance : public AST::Communicator {
+public:
+  GameInstance(Room &room, GameServer &server);
+  void loadGame(AST::AST &ast, AST::Environment env = AST::Environment{});
+  void runGame();
+  bool isRunning() { return !gameTask.isDone(); }
+
+  /// Only allow new message to be queued when the game requests it.
+  /// Returns false if fails to queue message.
+  bool queueMessage(const User &user, std::string message);
+
+  /// Send ouput message to a player
+  void sendToPlayer(const Player &player, std::string message);
+
+  void sendGlobalMessage(std::string message) override;
+
+  /// Get messages from a player.
+  /// Returns empty deque if no message is available.
+  std::deque<PlayerMessage> receiveFromPlayer(const Player &player);
+
+  /// Get info about players in the room
+  const std::vector<Player> &getPlayers() const { return players; }
+
+  bool isGameUnused() const { return room->getMembers().empty(); }
+
+private:
+  Room *room;
+  GameServer *server;
+  std::map<int, bool> playerMessageRequest;
+  std::vector<Player> players;
+  std::map<int, userid> playerIdMapping;
+  std::map<userid, int> reversePlayerIdMapping;
+  std::list<PlayerMessage> inboundMessageQueue;
+  coro::Task<> gameTask;
+};
