89591ea21fe7541a48664c9f4202509bc67006a7
jihos <jihos@sfu.ca>
2020-11-09 15:43:07 -0800

    initial

---
+++ AST/ExpressionParser.h
@@ -0,0 +1,13 @@
+// C++ program for expression tree
+#include<bits/stdc++.h>
+using namespace std;
+
+// The initial code from https://www.geeksforgeeks.org/expression-tree/
+
+parseExpression(std::string){
+    // BinaryOperation(left, right, operator)
+    // !players.elements.weapon =
+    // BinaryOperation(players , elements, dot)
+    // BinaryOperation(prevOperation, weapon)
+    // UnaryOperation(prevOperation, dot)
+}
---
+++ test/ExpressionParseTest.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "ExpressionParser.h"
+
+using namespace testing;
+
+TEST(ExpressionParseTest, initial) {
+    
+    
+}
