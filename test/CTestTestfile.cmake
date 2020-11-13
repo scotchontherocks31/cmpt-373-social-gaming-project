8c745002d0c712de31cf77484440f8d4132ebd8a
sarbjot-14 <sarbjot_14@hotmail.com>
2020-11-13 03:34:56 +0000

    removing garbage files

--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/social-gaming2/social-gaming/test
-# Build directory: /workspaces/social-gaming2/social-gaming/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "/workspaces/social-gaming2/social-gaming/bin/runAllTests")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/social-gaming2/social-gaming/test/CMakeLists.txt;15;add_test;/workspaces/social-gaming2/social-gaming/test/CMakeLists.txt;0;")
