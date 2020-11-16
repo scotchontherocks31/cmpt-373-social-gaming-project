fbb5eff06ac5ea6ae3929d75af3f0db01fabcb72
jihos <jihos@sfu.ca>
2020-11-15 17:37:48 -0800

    added parser

--- AST/ExpressionParser.cpp
+++ AST/ExpressionParser.cpp
@@ -1,6 +1,73 @@
 #include "ExpressionParser.h"
 
+#include <algorithm>
+#include <iostream>
 #include <map>
+//--------------------
+Type getStringToType(std::string str) {
+  // std::string::iterator end_pos = std::remove(str.begin(), str.end(), ' ');
+  // str.erase(end_pos, str.end());
+  if (stringToType.count(str) != 0) {
+    return stringToType[str];
+  } else {
+    return Type::ID;
+  }
+}
+
+int lookAhead(std::string str, int end) {
+
+  if (str[end + 1] == '=') {
+    end += 2;
+  } else {
+    end += 1;
+  }
+
+  return end;
+}
+
+TokenType toTokenType(std::string str, int begin, int end) {
+  std::string input = str.substr(begin, (end - begin));
+  Type type = getStringToType(input);
+  return TokenType(type, input);
+}
+
+std::vector<TokenType> parseToType(std::string str) {
+  std::string::iterator end_pos = std::remove(str.begin(), str.end(), ' ');
+  str.erase(end_pos, str.end());
+
+  std::vector<TokenType> tokens;
+
+  int begin = 0;
+  int end = 0;
+
+  while (begin < str.length()) {
+
+    if (tokenTrigger.count(str[end]) != 0) { // triggers on . , = , < , etc...
+      if ((end - begin) == 0) { // if very next string is an opertor
+
+        end = lookAhead(str, end);
+        tokens.push_back(toTokenType(str, begin, end));
+        begin = end;
+      } else {
+        tokens.push_back(toTokenType(str, begin, end));
+        begin = end;
+
+        end = lookAhead(str, end);
+
+        tokens.push_back(toTokenType(str, begin, end));
+        begin = end;
+      }
+
+    } else if (end >= str.length()) {
+      tokens.push_back(toTokenType(str, begin, str.length()));
+      break;
+    } else {
+      end++;
+    }
+  }
+  return tokens;
+}
+//--------------------------
 
 std::vector<std::string> parseExpression(std::string str) {
   struct Visitor;
--- AST/ExpressionParser.h
+++ AST/ExpressionParser.h
@@ -9,6 +9,52 @@
 #include <unordered_map>
 #include <vector>
 
+enum class Type { // name it better
+  OPENPAR,
+  CLOSEPAR,
+  ID,
+  DOT,
+  EQUALS,
+  NOTEQUALS,
+  GREATER,
+  GREATEREQUALS,
+  LESS,
+  LESSEQUALS,
+  NOT,
+  COMMA,
+};
+
+static std::map<char, int> tokenTrigger = { // name it better
+    {'(', 1}, {')', 1}, {'.', 1}, {'=', 2},
+    {'!', 2}, {'<', 2}, {'>', 2}, {',', 1}};
+
+static std::map<std::string, Type> stringToType = { // name it better
+    {"(", Type::OPENPAR},
+    {")", Type::CLOSEPAR},
+    {".", Type::DOT},
+    {"==", Type::EQUALS},
+    {"!=", Type::NOTEQUALS},
+    {">", Type::GREATER},
+    {">=", Type::GREATEREQUALS},
+    {"<", Type::LESS},
+    {"<=", Type::LESSEQUALS},
+    {"!", Type::NOT},
+    {",", Type::COMMA}};
+
+class TokenType {
+public:
+  TokenType(Type t, std::string s) : type{t}, value{s} {}
+  std::pair<Type, std::string> getTypeValue() {
+    return std::pair<Type, std::string>(type, value);
+  }
+  std::string getValue() { return value; }
+  Type getType() { return type; }
+
+private:
+  Type type;
+  std::string value;
+};
+
 enum class OperationType {
   DOT,
   EQUALS,
@@ -28,5 +74,5 @@ static std::map<std::string, OperationType> stringToOperator = {
 
 std::vector<std::string> parseExpression(std::string str);
 std::vector<std::string> expressionParser(std::string str);
-
+std::vector<TokenType> parseToType(std::string str);
 #endif
