410532b14114911bd535610df30466f4243f12dc
vnadtoch <vnadtoch@sfu.ca>
2020-11-22 07:35:02 +0000

    Solved bug with maps

--- tools/chatserver/GameServer.h
+++ tools/chatserver/GameServer.h
@@ -71,12 +71,10 @@ private:
   std::map<userid, User> users;
   std::deque<Message> inboundMessages;
   std::deque<Message> outboundMessages;
-  std::map<std::string, GameServer::Command> strToCommandMap;
-  std::map<std::string, GameServer::Command> strToGameCommandMap;
-  std::map<GameServer::Command, std::function<functionType>>
-      commandToFunctionMap;
-  std::map<GameServer::Command, std::function<functionType>>
-      commandToGameFunctionMap;
+  AnyStrToCommandM strToCommandMap;
+  AnyStrToCommandM strToGameCommandMap;
+  AnyCommandToFunctionM commandToFunctionMap;
+  AnyCommandToFunctionM commandToGameFunctionMap;
   bool running = false;
   void onConnect(Connection c);
   void onDisconnect(Connection c);
