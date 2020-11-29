d0a97c18a63aa5442e9d058647ce0cb48cdb53ee
jihos <jihos@sfu.ca>
2020-11-28 21:09:13 -0800

    Refactored Expression Parser. Inspired by looking at Group B's code during the code review.

--- AST/ExpressionParser.cpp
+++ AST/ExpressionParser.cpp
@@ -1,9 +1,38 @@
 #include "ExpressionParser.h"
 #include "RDP.h"
 #include <algorithm>
+#include <exception>
 #include <iostream>
 #include <map>
-//--------------------
+#include <regex>
+//--------regex------------
+
+std::regex ID{"^[A-Za-z0-9]+"};
+
+std::regex DOT{"^[.]"}; // These 3 MUST be in Square Brackets, beccause they
+                        // have a special case with round bracket
+std::regex OPENPAR{"^[(]"};
+std::regex CLOSEPAR{"^[)]"};
+
+std::regex EQUALS{"^(==)"};
+std::regex NOTEQUALS{"^(!=)"};
+std::regex NOT{"^(!)"};
+std::regex GREATER{"^(>)"};
+std::regex GREATEREQUALS{"^(>=)"};
+std::regex LESS{"^(>)"};
+std::regex LESSEQUALS{"^(<=)"};
+std::regex COMMA{"^(,)"};
+
+//--------------------------
+
+class myexception : public std::exception {
+  virtual const char *what() const throw() { return "Expression is invalid"; }
+};
+
+std::vector<std::regex> expressionList = {
+    ID,      DOT,           OPENPAR, CLOSEPAR,   EQUALS, NOTEQUALS,
+    GREATER, GREATEREQUALS, LESS,    LESSEQUALS, NOT,    COMMA};
+
 Type getStringToType(std::string str) {
   if (stringToType.count(str) != 0) {
     return stringToType[str];
@@ -12,54 +41,33 @@ Type getStringToType(std::string str) {
   }
 }
 
-int lookAhead(std::string str, int end) {
-  if (str[end + 1] == '=') {
-    end += 2;
-  } else {
-    end += 1;
-  }
-  return end;
-}
+std::optional<std::string> getFirstMatch(std::string str) {
+  std::smatch match;
 
-TokenType toTokenType(std::string str, int begin, int end) {
-  std::string input = str.substr(begin, (end - begin));
-  Type type = getStringToType(input);
-  return TokenType(type, input);
+  for (auto &expr : expressionList) {
+    if (std::regex_search(str, match, expr)) {
+      return match.str(0);
+    }
+  }
+  return std::nullopt;
 }
 
 std::vector<TokenType> parseToType(std::string str) {
   std::string::iterator end_pos = std::remove(str.begin(), str.end(), ' ');
   str.erase(end_pos, str.end());
-
   std::vector<TokenType> tokens;
 
-  int begin = 0;
-  int end = 0;
+  while (!str.empty()) {
+    try {
+      std::string match = getFirstMatch(str).value();
+      TokenType t(getStringToType(match), match);
+      tokens.push_back(t);
+      str = str.substr(match.length());
 
-  while (begin < str.length()) {
-
-    if (tokenTrigger.count(str[end]) != 0) { // triggers on . , = , < , etc...
-      if ((end - begin) == 0) { // if very next string is an opertor
-
-        end = lookAhead(str, end);
-        tokens.push_back(toTokenType(str, begin, end));
-        begin = end;
-      } else {
-        tokens.push_back(toTokenType(str, begin, end));
-        begin = end;
-
-        end = lookAhead(str, end);
-
-        tokens.push_back(toTokenType(str, begin, end));
-        begin = end;
-      }
-
-    } else if (end >= str.length()) {
-      tokens.push_back(toTokenType(str, begin, str.length()));
-      break;
-    } else {
-      end++;
+    } catch (const std::bad_optional_access &e) { // nullopt
+      throw myexception();
     }
   }
+
   return tokens;
 }
